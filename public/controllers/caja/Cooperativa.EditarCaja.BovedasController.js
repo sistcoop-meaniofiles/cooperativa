7d1ad4029fb8a79311308ab25e975d0fca22378c
Carlos <carlosthe19916@gmail.com>
2015-05-12 12:05:15 -0500

    add bovedaCaja

--- public/controllers/caja/Cooperativa.EditarCaja.BovedasController.js
+++ public/controllers/caja/Cooperativa.EditarCaja.BovedasController.js
@@ -2,7 +2,7 @@
 
 /* jshint -W098 */
 angular.module('mean.cooperativa').controller('Cooperativa.EditarCaja.BovedasController',
-    function($scope, $state, $filter, caja, toastr, SGAgencia, SGBoveda, SGDialog ){
+    function ($scope, $state, $filter, caja, toastr, SGAgencia, SGBoveda, SGBovedaCaja, SGDialog) {
 
         $scope.view = {
             caja: caja
@@ -19,42 +19,42 @@ angular.module('mean.cooperativa').controller('Cooperativa.EditarCaja.BovedasCon
             boveda: undefined
         };
 
-        $scope.loadCombo = function() {
-            SGAgencia.$findByCodigo($scope.view.caja.agencia).then(function(agencia){
+        $scope.loadCombo = function () {
+            SGAgencia.$findByCodigo($scope.view.caja.agencia).then(function (agencia) {
                 $scope.combo.boveda = SGBoveda.$search({agencia: agencia.codigo}).$object;
             });
         };
         $scope.loadCombo();
 
-        $scope.loadBovedasAsignadas = function(){
+        $scope.loadBovedasAsignadas = function () {
             $scope.view.loaded.bovedasAsignadas = $scope.view.caja.$getBovedaCajas().$object;
         };
         $scope.loadBovedasAsignadas();
 
-        $scope.submit = function(){
-            if($scope.form.$valid){
+        $scope.submit = function () {
+            if ($scope.form.$valid) {
 
-                if($scope.view.caja.estado === false){
+                if ($scope.view.caja.estado === false) {
                     toastr.info("Caja inactiva, no se puede actualizar.");
                     return;
                 }
-                if($scope.view.caja.abierto){
+                if ($scope.view.caja.abierto) {
                     toastr.warn('Caja abierta, debe cerrarla antes de vincular boveda.');
                     return;
                 }
 
-                for(var i=0; i<$scope.view.loaded.bovedasAsignadas.length; i++){
-                    if($scope.view.loaded.bovedasAsignadas[i].boveda.id == $scope.combo.selected.boveda.id){
+                for (var i = 0; i < $scope.view.loaded.bovedasAsignadas.length; i++) {
+                    if ($scope.view.loaded.bovedasAsignadas[i].boveda.id == $scope.combo.selected.boveda.id) {
                         toastr.warning('Boveda ya fue asignada');
                         return;
                     }
-                    if($scope.view.loaded.bovedasAsignadas[i].boveda.moneda == $scope.combo.selected.boveda.moneda){
+                    if ($scope.view.loaded.bovedasAsignadas[i].boveda.moneda == $scope.combo.selected.boveda.moneda) {
                         toastr.warn('Boveda con moneda ' + $scope.combo.selected.boveda.moneda + ' ya fue asignada');
                         return;
                     }
                 }
 
-                SGDialog.confirm('Vincular', '¿Estas seguro de vincular la caja para la boveda?', function() {
+                SGDialog.confirm('Vincular', '¿Estas seguro de vincular la caja para la boveda?', function () {
 
                     var bovedaCaja = {
                         boveda: {
@@ -63,17 +63,23 @@ angular.module('mean.cooperativa').controller('Cooperativa.EditarCaja.BovedasCon
                     };
 
                     $scope.view.caja.$addBovedaCaja(bovedaCaja).then(
-                        function(response){
+                        function (response) {
                             toastr.success("Boveda vinculada");
-                            if(angular.isDefined($scope.view.loaded.bovedasAsignadas)){
-                                $scope.view.loaded.bovedasAsignadas.push({boveda: $scope.combo.selected.boveda});
+                            if (angular.isDefined($scope.view.loaded.bovedasAsignadas)) {
+                                $scope.view.loaded.bovedasAsignadas.push({
+                                    boveda: $scope.combo.selected.boveda,
+                                    saldo: 0
+                                });
                             } else {
                                 $scope.view.loaded.bovedasAsignadas = [];
-                                $scope.view.loaded.bovedasAsignadas.push({boveda: $scope.combo.selected.boveda});
+                                $scope.view.loaded.bovedasAsignadas.push({
+                                    boveda: $scope.combo.selected.boveda,
+                                    saldo: 0
+                                });
                             }
                             $scope.combo.selected.boveda = undefined;
                         },
-                        function error(err){
+                        function error(err) {
                             toastr.error(err.data.message);
                         }
                     );
@@ -83,21 +89,32 @@ angular.module('mean.cooperativa').controller('Cooperativa.EditarCaja.BovedasCon
 
         };
 
-        $scope.removeBoveda = function($index, item){
-            if($scope.view.caja.abierto){
+        $scope.removeBoveda = function ($index, item) {
+            if ($scope.view.caja.abierto) {
                 toastr.warning('Caja abierta, debe cerrarla antes de desvincular boveda');
                 return;
             }
 
-            for(var i=0; i<$scope.view.loaded.bovedasAsignadas.length; i++){
-                if($scope.view.loaded.bovedasAsignadas[i].saldo !== 0){
+            for (var i = 0; i < $scope.view.loaded.bovedasAsignadas.length; i++) {
+                if ($scope.view.loaded.bovedasAsignadas[i].saldo !== 0) {
                     toastr.warning('Caja tiene saldo diferente de 0.00, no puede desvincularla');
                     return;
                 }
             }
 
-            SGDialog.confirm('Eliminar', '¿Estas seguro de desvincular la boveda para la caja?. Debes de asegurarte que no existe saldo en caja en la moneda de la boveda para continuar.', function() {
-                alert('Metodo no implementado');
+            SGDialog.confirm('Eliminar', '¿Estas seguro de desvincular la boveda para la caja?. Debes de asegurarte que no existe saldo en caja para la boveda.', function () {
+
+                SGBovedaCaja.$new(item.id).$disable().then(
+                    function (response) {
+                        toastr.success("Boveda desvinculada");
+                        $scope.view.loaded.bovedasAsignadas.splice($index, 1);
+                        $scope.combo.selected.boveda = undefined;
+                    },
+                    function error(err) {
+                        toastr.error(err.data.message);
+                    }
+                );
+
             });
 
         };
